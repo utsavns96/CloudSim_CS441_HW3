668e98385004e409a84af57506974375480a1a4d
utsavsharma <utsavns96@gmail.com>
2022-11-17 01:03:06 -0600

    Adding logging to VmAllocation

--- src/main/scala/VmAllocation.scala
+++ src/main/scala/VmAllocation.scala
@@ -1,3 +1,4 @@
+import HelperUtils.CreateLogger
 import org.cloudbus.cloudsim.allocationpolicies.{VmAllocationPolicyFirstFit, VmAllocationPolicyRoundRobin}
 import org.cloudbus.cloudsim.brokers.{DatacenterBroker, DatacenterBrokerSimple}
 import org.cloudsimplus.builders.tables.{CloudletsTableBuilder, TextTableColumn}
@@ -21,8 +22,10 @@ import com.typesafe.config.{Config, ConfigFactory}
 import java.util
 
 object VmAllocation {
+  val logger = CreateLogger(classOf[VmAllocation.type])
   def runVmAllocation: Unit = {
     //Getting our configs
+    logger.info("*****-----------------------------------------Starting VmAllocation Simulation-----------------------------------------*****")
     val config: Config = ConfigFactory.load("VmAllocation.conf")
     val hostconfig = config.getConfig("HostConfig")
     val vmconfig = config.getConfig("VMConfig")
@@ -31,18 +34,24 @@ object VmAllocation {
     val simulation = new CloudSim
     //creating a new datacenter
     val datacenter0 = createDatacenter(hostconfig.getInt("HOSTS"), hostconfig.getInt("HOST_PES"), hostconfig.getInt("HOST_MIPS"), hostconfig.getInt("HOST_RAM"), hostconfig.getInt("HOST_BW"), hostconfig.getInt("HOST_STORAGE"), simulation)
+    logger.info("Datacenter0 created")
     //creating a new broker to manage our VMs and Cloudlets
     val broker0 = new DatacenterBrokerSimple(simulation)
+    logger.info("Broker0 created")
     //creating our VMS
     val vmList = createVms(hostconfig.getInt("HOST_MIPS"), vmconfig.getInt("VMS"), vmconfig.getInt("VM_PES"), vmconfig.getInt("VM_RAM"), vmconfig.getInt("VM_BW"), vmconfig.getInt("VM_SIZE"))
     //copying our VMs to use later for the second simulation
     val vmList1 = vmList
+    logger.info("VMs created")
     //creating our new cloudlets
     val cloudletList = createCloudlets(new UtilizationModelDynamic(0.5), cloudletconfig.getInt("CLOUDLETS"), cloudletconfig.getInt("CLOUDLET_LENGTH"), cloudletconfig.getInt("CLOUDLET_PES"), cloudletconfig.getInt("CLOUDLET_SIZE"))
+    logger.info("Cloudlets created")
     //submitting our VMs
     broker0.submitVmList(vmList)
+    logger.info("VMs submitted")
     //setting our datacenter to use the First Fit policy for VM allocation.
     datacenter0.setVmAllocationPolicy(new VmAllocationPolicyFirstFit)
+    logger.info("Simulating first datacenter using VmAllocationPolicyFirstFit")
     //submitting our cloudlets
     broker0.submitCloudletList(cloudletList)
     //starting the first simulation
@@ -53,6 +62,7 @@ object VmAllocation {
     new CloudletsTableBuilder(finishedCloudlets.asJava).addColumn(new TextTableColumn("RAM Utilization", "Percentage"), (cloudlet: Cloudlet) => BigDecimal(cloudlet.getUtilizationOfRam() * 100.0).setScale(2, BigDecimal.RoundingMode.HALF_UP))
       .addColumn(new TextTableColumn("CPU Utilization", "Percentage"), (cloudlet: Cloudlet) => BigDecimal(cloudlet.getUtilizationOfCpu() * 100.0).setScale(2, BigDecimal.RoundingMode.HALF_UP))
       .build()
+    logger.info("Starting execution of second datacenter")
     /**
      *
      *  Setting up everything for our second simulation
@@ -62,16 +72,22 @@ object VmAllocation {
     val simulation1 = new CloudSim
     //creating the second datacenter
     val datacenter1 = createDatacenter(hostconfig.getInt("HOSTS"), hostconfig.getInt("HOST_PES"), hostconfig.getInt("HOST_MIPS"), hostconfig.getInt("HOST_RAM"), hostconfig.getInt("HOST_BW"), hostconfig.getInt("HOST_STORAGE"), simulation1)
+    logger.info("Second datacenter created")
     //creating a second broker to manage our VMs and Cloudlets for the second simulation
     val broker1 = new DatacenterBrokerSimple(simulation1)
+    logger.info("second broker created")
     //creating our new cloudlets
     val cloudletList1 = createCloudlets(new UtilizationModelDynamic(0.5), cloudletconfig.getInt("CLOUDLETS"), cloudletconfig.getInt("CLOUDLET_LENGTH"), cloudletconfig.getInt("CLOUDLET_PES"), cloudletconfig.getInt("CLOUDLET_SIZE"))
+    logger.info("New cloudlets created for second datacenter")
     //submitting our VMs to the second simulation
     broker1.submitVmList(vmList1)
+    logger.info("VMs submitted to second datacenter")
     //setting our second datacenter to also use the First Fit policy for VM allocation.
     datacenter1.setVmAllocationPolicy(new VmAllocationPolicyRoundRobin)
+    logger.info("Simulating first datacenter using VmAllocationPolicyRoundRobin")
     //submitting our cloudlets
     broker1.submitCloudletList(cloudletList1)
+    logger.info("Cloudlets submitted to second datacenter")
     //starting the second simulation
     simulation1.start
     //gathering the results of our second simulation
@@ -80,6 +96,7 @@ object VmAllocation {
     new CloudletsTableBuilder(finishedCloudlets1.asJava).addColumn(new TextTableColumn("RAM Utilization", "Percentage"), (cloudlet: Cloudlet) => BigDecimal(cloudlet.getUtilizationOfRam() * 100.0).setScale(2, BigDecimal.RoundingMode.HALF_UP))
       .addColumn(new TextTableColumn("CPU Utilization", "Percentage"), (cloudlet: Cloudlet) => BigDecimal(cloudlet.getUtilizationOfCpu() * 100.0).setScale(2, BigDecimal.RoundingMode.HALF_UP))
       .build()
+    logger.info("Simulation finished")
   }
 
   /**
@@ -87,9 +104,10 @@ object VmAllocation {
    */
   private def createDatacenter(host: Int, pes: Int, mpis: Int, ram: Int, bw: Int, storage: Int, sim: CloudSim) = {
     val hostList = new util.ArrayList[Host](host)
-    (1 to host).map { _ =>
+    (1 to host).map { i =>
       val host = createHost(new VmSchedulerTimeShared(), pes, mpis, ram, bw, storage)
       hostList.add(host)
+      logger.debug("Host "+i+" created")
     }
     new DatacenterSimple(sim, hostList)
   }
@@ -100,14 +118,10 @@ object VmAllocation {
   private def createHost(vmscheduler: VmScheduler, pes: Int, mips: Int, ram: Int, bw: Int, storage: Int) = {
     val peList = new util.ArrayList[Pe](pes)
     //List of Host's CPUs (Processing Elements, PEs)
-    //Uses a PeProvisionerSimple by default to provision PEs for VMs
     (1 to pes).map { _ =>
       peList.add(new PeSimple(mips))
     }
-    /*
-            Uses ResourceProvisionerSimple by default for RAM and BW provisioning
-            and VmSchedulerSpaceShared for VM scheduling.
-            */ new HostSimple(ram, bw, storage, peList).setVmScheduler(vmscheduler)
+     new HostSimple(ram, bw, storage, peList).setVmScheduler(vmscheduler)
   }
 
   /**
