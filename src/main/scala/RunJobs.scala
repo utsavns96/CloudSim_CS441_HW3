96db90ce8f01fd55a28f768a9dad04094b9e723f
utsavsharma <utsavns96@gmail.com>
2022-11-17 19:16:01 -0600

    Updating RunJobs

--- src/main/scala/RunJobs.scala
+++ src/main/scala/RunJobs.scala
@@ -1,9 +1,20 @@
 object RunJobs {
   def main(args: Array[String]): Unit = {
-    //VmAllocation.runVmAllocation
-    //VmUtilAndSchedule.runVmScheduler
-    CloudProvider.runCloud
-    //ScalingDatacenter.runScaling
+    println("*-------------Cloud Simulator-------------*")
+    println("Press [1] to run VmAllocation Simulation")
+    println("Press [2] to run VmUtilAndSchedule Simulation")
+    println("Press [3] to run CloudProvider Simulation")
+    println("Press [4] to run ScalingDatacenter Simulation")
+    println("Press [5] to run IaasPaasSaas Simulation")
+    val a = scala.io.StdIn.readInt()
+    a match{
+      case 1 => VmAllocation.runVmAllocation
+      case 2 => VmUtilAndSchedule.runVmScheduler
+      case 3 => CloudProvider.runCloud
+      case 4 => ScalingDatacenter.runScaling
+      case 5 => println("Not implemented yet")
+      case _ => println("Invalid Input")
+    }
   }
 
 }
